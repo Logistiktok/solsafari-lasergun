5a127872391b6a6ccd2d12c454c7468d9ad9d066
Daniel Graungaard <graungaard@cs,au.dk>
2021-04-04 01:16:06 +0200

    Added running lights lol so pretty

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,16 @@
 #include <IRLibCombo.h>   
 #include <Arduino.h>
 
+
 #define GUN_INPUT 4
+#define LED_1 A2
+#define LED_2 A0
+#define LED_3 14
+#define LED_4 10
+
 
 #define DELAY_BETWEEN_SHOTS 200 // in ms
+#define DELAY_BETWEEN_LIGHTS 50
 
 boolean isReadyToShoot = true;
 unsigned long timestamp = 0;
@@ -17,8 +24,44 @@ IRsendRC5 mySender;
  
 void setup() {
   pinMode(GUN_INPUT, INPUT_PULLUP);
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+  pinMode(LED_4, OUTPUT);
+
 }
  
+void updateLights() {
+  
+  if (millis() - timestamp < DELAY_BETWEEN_LIGHTS) {
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, LOW);
+    digitalWrite(LED_4, LOW);
+  } else if((millis() - timestamp) < DELAY_BETWEEN_LIGHTS * 2) {
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, HIGH);
+    digitalWrite(LED_3, LOW);
+    digitalWrite(LED_4, LOW);
+  } else if((millis() - timestamp) < DELAY_BETWEEN_LIGHTS * 3) {
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, HIGH);
+    digitalWrite(LED_4, LOW);
+  } else if((millis() - timestamp) < DELAY_BETWEEN_LIGHTS * 4) {
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, LOW);
+    digitalWrite(LED_4, HIGH);
+  } else {
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, LOW);
+    digitalWrite(LED_4, LOW);
+  }
+
+}
+
 void loop() {
     //send a code every time a character is received from the serial port
     //Sony DVD power A8BCA
@@ -28,13 +71,12 @@ void loop() {
       mySender.send(0x7f9d);
       isReadyToShoot = false;
       timestamp = millis();
-
      } 
      
      if ((millis() - timestamp) > DELAY_BETWEEN_SHOTS && button_state) {
        isReadyToShoot = true;
      }
+
+     updateLights();
      
-
-
-} 
+}
